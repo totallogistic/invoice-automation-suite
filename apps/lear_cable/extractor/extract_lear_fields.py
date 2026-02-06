482a690d8add2d75c78820aca767dfd0fc8c3325
Miguel del Pino <mapinoma@gmail.com>
2026-02-06 18:40:00 +0100

    Push change in extractor to validate wf

--- apps/lear_cable/extractor/extract_lear_fields.py
+++ apps/lear_cable/extractor/extract_lear_fields.py
@@ -11,7 +11,7 @@ from decimal import Decimal, ROUND_HALF_UP
 from pathlib import Path
 from typing import Iterable, Optional
 
-SCRIPT_VERSION = "2026-02-6.v20"
+SCRIPT_VERSION = "2026-02-6.v21"
 
 try:
     from pypdf import PdfReader
